525447c8b714d0e3113b4631dba8b69364dffa5d
Alex Di Corrado <dicorradoalex@gmail.com>
2026-04-03 15:53:31 +0200

    feat: add CategoryDTO to codebase

---
+++ src/main/java/com/lipari/Academy2026/dto/CategoryDTO.java
@@ -0,0 +1,8 @@
+package com.lipari.Academy2026.dto;
+
+
+import java.util.UUID;
+
+public record CategoryDTO(UUID id, String name) {
+
+}
---
+++ src/main/java/com/lipari/Academy2026/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.lipari.Academy2026.mapper;
+
+import com.lipari.Academy2026.dto.ProductDTO;
+import com.lipari.Academy2026.entity.ProductEntity;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    ProductDTO toDto(ProductEntity pEntity);
+
+    ProductEntity toEntity(ProductDTO pDto);
+
+    List<ProductDTO> toDtoList(List<ProductEntity> pEntities);
+
+}
