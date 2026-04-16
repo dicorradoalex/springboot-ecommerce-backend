5a6d64423fb747fbd00e4ebe3d6282a61e7c6fb1
Alex Di Corrado <dicorradoalex@gmail.com>
2026-04-16 18:31:37 +0200

    feat(UserRepository): add findByEmail for security

--- src/main/java/com/lipari/Academy2026/repository/UserRepository.java
+++ src/main/java/com/lipari/Academy2026/repository/UserRepository.java
@@ -5,13 +5,18 @@ import com.lipari.Academy2026.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
-    // Grazie a JpaRepository: save(), findById(), findAll(), deleteById()
-    public boolean existsByEmail(String email);
+    // Per la registrazione - Restituisce true/false per evitare duplicati
+    boolean existsByEmail(String email);
 
-
+    // Per il login: restituisce l'utente completo o un Optional vuoto se non esiste
+    Optional<UserEntity> findByEmail(String email);
 }
+
+// Grazie a JpaRepository: save(), findById(), findAll(), deleteById()
+
